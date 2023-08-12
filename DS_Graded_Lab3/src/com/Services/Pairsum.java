c5e22d4040ce83708e014e81c2c8fc2bd880bd03
codersubho <89599447+codersubho@users.noreply.github.com>
2023-08-12 12:36:54 +0530

    Update Pairsum.java

--- DS_Graded_Lab3/src/com/Services/Pairsum.java
+++ DS_Graded_Lab3/src/com/Services/Pairsum.java
@@ -17,7 +17,7 @@ public class Pairsum {
 			return flag;
 		}
 		BinaryNode n = bst.search(targetsum - temp.data);
-		if (n != null) {
+		if (n != null && n.data != targetsum/2) {
 			System.out.println("Two such nodes in the binary search tree whose sum is " + targetsum + " are " + "("
 					+ temp.data + " , " + n.data + ")");
 			flag = true;
